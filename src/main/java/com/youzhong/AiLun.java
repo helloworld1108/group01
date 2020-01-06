a5bc654e6f1c862b7dc3ae9f1d380da91622a370
wangzhi <993720368@qq.com>
2020-01-06 14:30:11 +0800

    艾伦做第3次修改 构造

--- src/main/java/com/youzhong/AiLun.java
+++ src/main/java/com/youzhong/AiLun.java
@@ -1,4 +1,13 @@
 package com.youzhong;
 
 public class AiLun {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
